65af5ae0c93f907f16e035400d54a964489338dd
Dabug123 <clarkbrunswick55@gmail.com>
2023-05-08 13:11:07 +0000

    fixes of fixes from fixes

--- crooked_panther.mk
+++ crooked_panther.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common crooked stuff.
-$(call inherit-product, device/google/pantah/aosp_cheetah.mk)
-$(call inherit-product, device/google/pantah/device-crooked.mk)
-$(call inherit-product, device/google/gs201/crooked_common.mk)
+$(call inherit-product, vendor/crooked/config/common.mk)
+$(call inherit-product, vendor/crooked/config/gsm.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
 $(call inherit-product, device/google/pantah/device-crooked.mk)
+$(call inherit-product, device/google/gs201/crooked_common.mk)
 
 # Parts
 $(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
@@ -25,4 +25,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=panther \
     PRIVATE_BUILD_DESC="panther-user 13 TQ2A.230505.002 9891397 release-keys"
 
-BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGEPRINT)
+BUILD_FINGERPRINT := google/panther/panther:13/TQ2A.230505.002/9891397:user/release-keys
